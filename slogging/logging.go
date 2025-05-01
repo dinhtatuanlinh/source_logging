a70cb1a91e8c731bdcc959869ad52bba1e20eb0e
dinhtatuanlinh <linh.dinh@zen8labs.com>
2025-05-01 14:26:15 +0700

    add operator username

--- slogging/logging.go
+++ slogging/logging.go
@@ -9,31 +9,37 @@ import (
 type Logger struct {
 	requestID string
 	apiID     string
+	operator  string
 }
 
 const (
 	XRequestID = "X-Request-ID"
 	APIID      = "api_id"
+	XOperator  = "x-operator"
 )
 
 func New(ctx context.Context) *Logger {
 	requestID, _ := ctx.Value(XRequestID).(string)
 	apiID, _ := ctx.Value(APIID).(string)
+	operator, _ := ctx.Value(XOperator).(string)
 	return &Logger{
 		requestID: requestID,
 		apiID:     apiID,
+		operator:  operator,
 	}
 }
 
 func (l *Logger) Info() *zerolog.Event {
 	return log.Info().
 		Str(XRequestID, l.requestID).
-		Str(APIID, l.apiID)
+		Str(APIID, l.apiID).
+		Str(XOperator, l.operator)
 }
 
 func (l *Logger) Error(err error) *zerolog.Event {
 	return log.Error().
 		Str(XRequestID, l.requestID).
 		Str(APIID, l.apiID).
+		Str(XOperator, l.operator).
 		Err(err)
 }
